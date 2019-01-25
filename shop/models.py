68ad61cc71c2eef4742c54c6a13ddb0041aeeab2
viseth89 <viseth89@gmail.com>
2019-01-24 21:18:09 -0600

    added models understanding db

--- shop/models.py
+++ shop/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=250, unique=True)
     slug = models.SlugField(max_length=250, unique=True)
-    description = model.TextField(blank=True)
+    description = models.TextField(blank=True)
     image = models.ImageField(upload_to='category', blank=True)
 
     class Meta: 
@@ -21,7 +21,7 @@ class Product(models.Model):
     description = models.TextField(blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='product', blank=unique)
+    image = models.ImageField(upload_to='product', blank=True)
     stock = models.IntegerField()
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
