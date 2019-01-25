b8e535954a9639ecb000e5ca3f46fbc10c619271
viseth89 <viseth89@gmail.com>
2019-01-25 10:52:51 -0600

    using slug?, confused about links, debugging

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse
+from .models import Category,Product
+from django.core.paginator import Paginator, EmptyPage, InvalidPage
+from django.contrib.auth.models import Group, User
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate, logout
 
-# Create your views here.
+
 def index(request):
-    return render(request, 'shop/index.html')
+	text_var = 'This is my first django app web page.'
+	return HttpResponse(text_var)
+
+#Category view
+
+def allProdCat(request, c_slug=None):
+    c_page = None
+    products_list = None
+    if c_slug!=None:
+        c_page = get_object_or_404(Category,slug=c_slug)
+        products_list = Product.objects.filter(category=c_page,available=True)
+    else:
+        products_list = Product.objects.all().filter(available=True)
+
+    return render(request, 'shop/category.html', {'category': c_page, 'products':products_list})
+
+def ProdCatDetail(request, c_slug, product_slug):
+    try:
+        Product.objects.get(category__slug=c_slug, slug=product_slug)
+    except Exception as e:
+        raise e
+    return render(request, 'shop/product.html', {'product':product})
